9d5d4fd3fdb62dbb38215625570bf6bd811f214d
noahsealy <noah.sealy8@gmail.com>
2025-06-24 21:57:40 -0300

    edit

--- learnpythondotorg/only_positives_lc.py
+++ learnpythondotorg/only_positives_lc.py
@@ -2,6 +2,6 @@
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
 # [expression for item in iterable if condition]
-newlist = [num for num in numbers if num > 0]
+newlist = [int(num) for num in numbers if num > 0]
 
 print(newlist)
