0a003ab620beb785f7883d7018a5ecf66425e35f
noahsealy <noah.sealy8@gmail.com>
2025-06-30 11:46:31 -0300

    review

---
+++ concept_notes/testing.py
@@ -0,0 +1,16 @@
+import copy
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+d = copy.copy(a)
+print(a is b) # True
+print(a is c) # False
+print(a is d) # False
+
+a = 1
+b = 1
+print(a is b) # True
+
+a = 1000000000
+b = 1000000000
+print(a is b) # True
