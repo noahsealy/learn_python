60d2a6c64373eff985cf549c84b06a40d84e9639
noahsealy <noah.sealy8@gmail.com>
2025-07-03 22:11:21 -0300

    reviewing

--- cracked_code/blaze.py
+++ cracked_code/blaze.py
@@ -1,7 +1,10 @@
+# built in functions
+# https://www.youtube.com/watch?v=YsOYMrBNGq8
 from timeit import Timer
 from numpy import random
 
-# built in functions
+# 1
+# faster because they're written in C!
 def sort(nums):
     return sorted(nums)
 
@@ -9,3 +12,27 @@ nums = random.randint(100, size=(10000))
 print(nums)
 y = Timer("sort(nums)", "from __main__ import sort, nums")
 print('Sorted timer: %f', y.timeit(number = 10))
+
+# 2
+# using a yield can save a lot of time and memory while unpacking large datasets
+def parse_csv(file_name):
+    for row in open(file_name, 'r'):
+        yield row
+
+def test_parse_csv():
+    csv_file = 'test.csv'
+    row_count = 0
+    for row in parse_csv(csv_file):
+        row_count += 1
+        # process data!
+    return row_count
+
+# 3
+# concurrency!
+import multiprocessing
+
+# 4
+# compile code with CPython
+
+# 5
+# use compiled frameworks, like numpy, pandas, and pillow
